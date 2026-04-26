04e339706970cd6daba75c92d9bdd05447ccf262
Shreyup Bhandari <shreyupbhandari@gmail.com>
2026-04-26 13:34:50 -0400

    Did some error handling and added comments to the myShell program.

--- myShell/myShell.c
+++ myShell/myShell.c
@@ -8,25 +8,33 @@ int main(int argc, char **argv)
 {
     
     printf("Hi! Welcome to ShreyShell!\n");
-    char *command=(char *)malloc(sizeof(char)*100);
-    char **argument=(char **) malloc(sizeof(char *)*10);
+    char *command=NULL;
+    char **argument=NULL;
+    if (NULL==(command=(char *)malloc(sizeof(char)*100)))
+    {
+        fprintf(stderr,"Malloc to the commmad failed!\n");
+    }
+    if (NULL==(argument=(char **) malloc(sizeof(char *)*10)))
+    {
+        fprintf(stderr,"Malloc to the command vector failed!\n");
+    }
     do
     {
         printf("ShreyupShell | ");
-        scanf(" %[^\n]",command);
+        scanf(" %[^\n]",command); //Read everything except a newline. Otherwise, scanf() terminates when a space appears. Also, a space is added before the % to account for buffer.
         char *commandCopy=strdup(command);
         int argumentSize=0;
         char *t=strtok(commandCopy," ");
         while(t!=NULL)
         {
             argument[argumentSize]=strdup(t);
-            t=strtok(NULL," ");
+            t=strtok(NULL," "); //NULL is passed from the second time because strdup() has a static internal pointer that keeps track of the delimeter.
             argumentSize++;
         }
-        argument[argumentSize]=NULL;
-        if (strcmp(argument[0],"exit")==0)
+        argument[argumentSize]=NULL;//The end of the vector passed to execvp() must terminate with NULL.
+        if (strcmp(argument[0],"exit")==0) //strcmp() compares two strings and returns 0 if the two strings are equal.
         {
-            free(commandCopy);
+            free(commandCopy); //strdup() is a malloc wrapper and has to be freed.
             for (int k=0; k<argumentSize; k++)
             {
                 free(argument[k]);
@@ -36,8 +44,9 @@ int main(int argc, char **argv)
         if (fork()==0)
         {
             printf("Executing %s command.\n",argument[0]);
-            execvp(argument[0],argument);
-            exit(1);
+            execvp(argument[0],argument); //The first argument is the program name, and the second is a vector that ends with a NULL pointer.
+            exit(1); //Since exec() replaces the calling program's memory sign with the new program, the code below it only runs if exec() fails.
+                    //exiting is crucial, otherwise the child process will be a zombie.
         }
         else
         {
